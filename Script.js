9e15aea1d77a3b7775697a3520107e2188a95164
HandsomeSphe <siphelelehandsome>
2023-03-10 11:23:28 +0200

    added second feature for slider

--- Script.js
+++ Script.js
@@ -79,82 +79,120 @@ servicesButtons.forEach(function (button) {
 });
 
 /*///////===== Functionality for Slider=====///////////*/
-const button__left = document.querySelector(".slider__buttons--left");
-const button__right = document.querySelector(".slider__buttons--right");
-const slides = document.querySelectorAll(".slider__slide");
-let currentSlide = 0;
-const maxSlide = slides.length - 1;
+const slider = function () {
+  const button__left = document.querySelector(".slider__buttons--left");
+  const button__right = document.querySelector(".slider__buttons--right");
+  const slides = document.querySelectorAll(".slider__slide");
+  let currentSlide = 0;
+  const maxSlide = slides.length - 1;
+  const dotsContainer = document.querySelector(".slider__dots ");
 
-const goToSlide = function (slidei) {
-  slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${100 * (i - slidei)}%)`;
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotsContainer.insertAdjacentHTML(
+        "beforeend",
+        `<div class="slider__dots--dot" data-slide="${i}"></div>`
+      );
+    });
+  };
+
+  const activeDots = function (slide) {
+    document.querySelectorAll(".slider__dots--dot").forEach(function (dot) {
+      dot.classList.remove("dot__active");
+    });
+
+    document
+      .querySelector(`.slider__dots--dot[data-slide="${slide}"]`)
+      .classList.add("dot__active");
+  };
+
+  const goToSlide = function (slidei) {
+    slides.forEach((slide, i) => {
+      slide.style.transform = `translateX(${100 * (i - slidei)}%)`;
+    });
+  };
+
+  //Next Slide
+  const nextSlide = () => {
+    if (currentSlide < maxSlide) {
+      currentSlide++;
+    } else {
+      currentSlide = 0;
+    }
+    goToSlide(currentSlide);
+    activeDots(currentSlide);
+  };
+
+  //Previous Slide
+  const prevSlide = () => {
+    if (currentSlide == 0) {
+      currentSlide = maxSlide;
+    } else {
+      currentSlide--;
+    }
+    goToSlide(currentSlide);
+    activeDots(currentSlide);
+  };
+  const init = function () {
+    goToSlide(0);
+    createDots();
+    activeDots(0);
+  };
+  init();
+
+  button__right.addEventListener("click", nextSlide);
+  button__left.addEventListener("click", prevSlide);
+  const myInterval = setInterval(nextSlide, 3000);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key == "ArrowRight") {
+      nextSlide();
+    }
+    if (e.key == "ArrowLeft") {
+      prevSlide();
+    }
   });
-};
 
-goToSlide(0);
+  dotsContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("slider__dots--dot")) {
+      const slide = e.target.dataset.slide;
 
-//Next Slide
-const nextSlide = () => {
-  if (currentSlide < maxSlide) {
-    currentSlide++;
-  } else {
-    currentSlide = 0;
-  }
-  goToSlide(currentSlide);
-};
-
-//Previous Slide
-const prevSlide = () => {
-  if (currentSlide == 0) {
-    currentSlide = maxSlide;
-  } else {
-    currentSlide--;
-  }
-  goToSlide(currentSlide);
-};
-
-button__right.addEventListener("click", nextSlide);
-button__left.addEventListener("click", prevSlide);
-const myInterval = setInterval(nextSlide, 3000);
-
-document.addEventListener("keydown", function (e) {
-  if (e.key == "ArrowRight") {
-    nextSlide();
-  }
-  if (e.key == "ArrowLeft") {
-    prevSlide();
-  }
-});
-
-////////Slide buttons functionality
-
-const bookServiceBtn = document.querySelectorAll(".slider__details--button");
-const sliderModal = document.querySelector(".slider__modal");
-let serviceInput = document.getElementById("service");
-let inputs = document.querySelectorAll(".slider__modal--input");
-let modalForm = document.querySelector(".slider__modal");
-const removeModal = document.querySelector(".remove__icon");
-
-bookServiceBtn.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    const title = e.target.dataset;
-    sliderModal.classList.toggle("showModal");
-    serviceInput.value = `${title.title}`;
-    clearInterval(myInterval);
+      goToSlide(slide);
+      activeDots(slide);
+    }
   });
-});
 
-removeModal.addEventListener("click", function () {
-  sliderModal.classList.remove("showModal");
-});
-/**/ ///////////// Footer inputs clearing *//////////
-const footerInputs = document.querySelectorAll(".contacts__form--input");
+  ////////Slide buttons functionality
 
-setTimeout(() => {
-  footerInputs.forEach((input) => {
-    input.value = " ";
+  const bookServiceBtn = document.querySelectorAll(".slider__details--button");
+  const sliderModal = document.querySelector(".slider__modal");
+  let serviceInput = document.getElementById("service");
+  let inputs = document.querySelectorAll(".slider__modal--input");
+  let modalForm = document.querySelector(".slider__modal");
+  const removeModal = document.querySelector(".remove__icon");
+
+  bookServiceBtn.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const title = e.target.dataset;
+      sliderModal.classList.toggle("showModal");
+      serviceInput.value = `${title.title}`;
+      clearInterval(myInterval);
+    });
   });
-}, 2500);
+
+  removeModal.addEventListener("click", function () {
+    sliderModal.classList.remove("showModal");
+  });
+  /**/ ///////////// Footer inputs clearing *//////////
+  const footerInputs = document.querySelectorAll(".contacts__form--input");
+
+  setTimeout(() => {
+    footerInputs.forEach((input) => {
+      input.value = " ";
+    });
+  }, 2500);
+};
+slider();
 
 /**/ //////////////////// FormSpree functionality *//////////////
 
